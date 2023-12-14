cf1ab134e80204c6d720eb08c79135c07547e618
rahulsinha2911 <rahulsinha4698@gmail.com>
2023-12-14 18:36:52 +0530

    credentials in env file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var http = require('http');
+require('dotenv').config();
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
+  console.log(process.env.REDIS_HOST)
   res.end('Hello World!');
 }).listen(8080);
